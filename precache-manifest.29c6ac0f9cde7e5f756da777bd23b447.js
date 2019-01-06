6c88c32f18c7b869fdbad694b3ea3cb8d99f5c7c
Jeremy Carter <Jeremy@JeremyCarter.ca>
2019-01-06 03:36:49 -0500

    Updates

---
+++ precache-manifest.29c6ac0f9cde7e5f756da777bd23b447.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5188d45c05cf86a485a4483dfc8e987",
+    "url": "/static/media/TelephoneSound1.b5188d45.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "76821e9e6f586e103924",
+    "url": "/static/js/main.76821e9e.chunk.js"
+  },
+  {
+    "revision": "da7ca80fab69630372e9",
+    "url": "/static/js/1.da7ca80f.chunk.js"
+  },
+  {
+    "revision": "76821e9e6f586e103924",
+    "url": "/static/css/main.1088e1cd.chunk.css"
+  },
+  {
+    "revision": "974c29c5954eba39ca6def0ad36d9a4f",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/telephonesound/precache-manifest.c9b0814d440e0f917d1467b2d5e5fffd.js"
+  "/precache-manifest.29c6ac0f9cde7e5f756da777bd23b447.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/telephonesound/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
