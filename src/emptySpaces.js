a8a57dccaac0816da5391bb1f961244495f0806e
Jeremy Carter <Jeremy@JeremyCarter.ca>
2019-02-08 17:46:30 -0500

    add google play and napster links

--- src/emptySpaces.js
+++ src/emptySpaces.js
@@ -25,8 +25,7 @@ export default class EmptySpaces extends Component {
 
           <ul>
             <li><a href="https://telephonesound.bandcamp.com/album/empty-spaces" target="_blank" rel="noopener noreferrer">Stream or buy on BandCamp</a></li>
-            <li>Stream or buy on Google Play Music Store (Coming Soon)</li>
-            {/* <li><a href="https://play.google.com/store/music/album/Telephone_Sound_Ellipsis_Deluxe?id=Bjhbpztgl4hfj75bd4opfp4grai" target="_blank" rel="noopener noreferrer">Stream or buy on Google Play Music Store</a></li> */}
+            <li><a href="https://play.google.com/store/music/album/Telephone_Sound_Empty_Spaces?id=Bnsveoyptamxwfpaln2gx6yej3m" target="_blank" rel="noopener noreferrer">Stream or buy on Google Play Music Store</a></li>
             <li>Buy on iTunes (Coming Soon)</li>
             {/* <li><a href="https://itunes.apple.com/us/album/ellipsis-deluxe/1435660482?uo=4&app=music&at=1001lry3&ct=dashboard&app=itunes&at=1001lry3&ct=dashboard" target="_blank" rel="noopener noreferrer">Buy on iTunes</a></li> */}
             <li><a href="https://www.amazon.com/gp/product/B07NDYFHF1" target="_blank" rel="noopener noreferrer">Buy on Amazon</a></li>
@@ -39,8 +38,7 @@ export default class EmptySpaces extends Component {
             <li><a href="https://www.deezer.com/album/86636832" target="_blank" rel="noopener noreferrer">Stream on Deezer</a></li>
             <li><a href="https://tidal.com/browse/album/103689661" target="_blank" rel="noopener noreferrer">Stream on Tidal</a></li>
             <li><a href="https://choon.co/albums/0qunzdv3kc5/empty-spaces" target="_blank" rel="noopener noreferrer">Stream on Choon</a></li>
-            <li>Stream on Napster (Coming Soon)</li>
-            {/* <li><a href="https://us.napster.com/artist/telephone-sound/album/ellipsis-deluxe" target="_blank" rel="noopener noreferrer">Stream on Napster</a></li> */}
+            <li><a href="https://us.napster.com/artist/telephone-sound/album/empty-spaces" target="_blank" rel="noopener noreferrer">Stream on Napster</a></li>
             <li><a href="https://www.kkbox.com/sg/en/album/Bpa.ppMs7Wazn0F2sh6T009H-index.html" target="_blank" rel="noopener noreferrer">Stream on KKBox</a></li>
             <li><a href="https://archive.org/details/telephone_sound-empty_spaces" target="_blank" rel="noopener noreferrer">Stream or download on Archive.org</a></li>
             <li><a href="https://www.facebook.com/telephonesound" target="_blank" rel="noopener noreferrer">Follow our Facebook page</a></li>
