05bd658ad24b3a6b0474aa83b71d13886bcc5260
Jeremy Carter <Jeremy@JeremyCarter.ca>
2019-02-10 11:30:36 -0500

    add itunes and apple music links

--- src/emptySpaces.js
+++ src/emptySpaces.js
@@ -26,15 +26,13 @@ export default class EmptySpaces extends Component {
           <ul>
             <li><a href="https://telephonesound.bandcamp.com/album/empty-spaces" target="_blank" rel="noopener noreferrer">Stream or buy on BandCamp</a></li>
             <li><a href="https://play.google.com/store/music/album/Telephone_Sound_Empty_Spaces?id=Bnsveoyptamxwfpaln2gx6yej3m" target="_blank" rel="noopener noreferrer">Stream or buy on Google Play Music Store</a></li>
-            <li>Buy on iTunes (Coming Soon)</li>
-            {/* <li><a href="https://itunes.apple.com/us/album/ellipsis-deluxe/1435660482?uo=4&app=music&at=1001lry3&ct=dashboard&app=itunes&at=1001lry3&ct=dashboard" target="_blank" rel="noopener noreferrer">Buy on iTunes</a></li> */}
+            <li><a href="https://itunes.apple.com/us/album/empty-spaces/1452079648?app=itunes&ign-mpt=uo%3D4" target="_blank" rel="noopener noreferrer">Buy on iTunes</a></li>
             <li><a href="https://www.amazon.com/gp/product/B07NDYFHF1" target="_blank" rel="noopener noreferrer">Buy on Amazon</a></li>
             <li><a href="https://www.youtube.com/watch?v=p2NIETwuiAQ&list=PLA9w7gX4C1R2vbq8Q2M3xqtB63xf6DKVG" target="_blank" rel="noopener noreferrer">Stream on YouTube</a></li>
             <li><a href="https://soundcloud.com/telephonesound/sets/empty-spaces" target="_blank" rel="noopener noreferrer">Stream or download on SoundCloud</a></li>
             <li><a href="https://audiomack.com/album/telephonesound/empty-spaces" target="_blank" rel="noopener noreferrer">Stream on Audiomack</a></li>
             <li><a href="https://open.spotify.com/album/0Gpi8Zfb6Zwj65Y0s7AFKd" target="_blank" rel="noopener noreferrer">Stream on Spotify</a></li>
-            <li>Stream on Apple Music (Coming Soon)</li>
-            {/* <li><a href="https://itunes.apple.com/us/album/ellipsis-deluxe/1435660482?uo=4&app=music&at=1001lry3&ct=dashboard" target="_blank" rel="noopener noreferrer">Stream on Apple Music</a></li> */}
+            <li><a href="https://itunes.apple.com/us/album/empty-spaces/1452079648?app=apple%20music&ign-mpt=uo%3D4" target="_blank" rel="noopener noreferrer">Stream on Apple Music</a></li>
             <li><a href="https://www.deezer.com/album/86636832" target="_blank" rel="noopener noreferrer">Stream on Deezer</a></li>
             <li><a href="https://tidal.com/browse/album/103689661" target="_blank" rel="noopener noreferrer">Stream on Tidal</a></li>
             <li><a href="https://choon.co/albums/0qunzdv3kc5/empty-spaces" target="_blank" rel="noopener noreferrer">Stream on Choon</a></li>
