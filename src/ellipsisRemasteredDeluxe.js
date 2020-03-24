3a106a80a4e4b802f702215a959d06e622d2bfef
Jeremy Carter <jeremy@jeremycarter.ca>
2020-03-24 14:40:18 -0400

    Remove amazon link temporarily.

--- src/ellipsisRemasteredDeluxe.js
+++ src/ellipsisRemasteredDeluxe.js
@@ -32,7 +32,7 @@ export default class EllipsisRemasteredDeluxe extends Component {
           <ul>
             <li><a href="https://telephonesound.bandcamp.com/album/ellipsis-remastered-deluxe" target="_blank" rel="noopener noreferrer">Stream or buy on BandCamp</a></li>
             <li><a href="https://play.google.com/store/music/album/Telephone_Sound_Ellipsis_Remastered_Deluxe?id=Be6uvln5i3jngnc7hfw6qhk7mui" target="_blank" rel="noopener noreferrer">Stream or buy on Google Play Music Store</a></li>
-            <li><a href="https://www.amazon.com/gp/product/B084RR3J81" target="_blank" rel="noopener noreferrer">Stream or buy on Amazon</a></li>
+            {/* <li><a href="https://www.amazon.com/gp/product/B084RR3J81" target="_blank" rel="noopener noreferrer">Stream or buy on Amazon</a></li> */}
             <li><a href="https://soundcloud.com/telephonesound/sets/ellipsis-remastered-deluxe" target="_blank" rel="noopener noreferrer">Stream or download on SoundCloud</a></li>
             <li><a href="https://audiomack.com/album/telephonesound/ellipsis-remastered-deluxe" target="_blank" rel="noopener noreferrer">Stream on Audiomack</a></li>
             <li><a href="https://music.youtube.com/playlist?list=OLAK5uy_n2ppYqCNO2K_sb3KJFWy5z7M5LoMtM4L4" target="_blank" rel="noopener noreferrer">Stream on YouTube Music</a></li>
