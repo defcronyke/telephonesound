2d29d67a3fe3d35d61a090eb313e9d270c88f0a7
Jeremy Carter <jeremy@jeremycarter.ca>
2020-03-23 20:12:00 -0400

    Update website for Ellipsis (Remastered Deluxe) album release.

--- src/ellipsisRemasteredDeluxe.js
+++ src/ellipsisRemasteredDeluxe.js
@@ -18,16 +18,32 @@ export default class EllipsisRemasteredDeluxe extends Component {
           <h4><Link to="/">Telephone Sound</Link> is an electronic music band from <a href="https://goo.gl/maps/68jtLij9kX72" target="_blank" rel="noopener noreferrer">Ottawa, Ontario, Canada</a>, consisting of partners <a href="https://prettyweird.ca" target="_blank" rel="noopener noreferrer">Fern Vale Volante</a> and <a href="https://eternalvoid.net" target="_blank" rel="noopener noreferrer">Jeremy Carter</a>.</h4>
 
           <a href="https://distrokid.com/hyperfollow/telephonesound/ellipsis-remastered-deluxe-2">
-            <h2>Coming Soon...</h2>
-
-            <h3>Ellipsis (Remastered Deluxe) is coming to your favourite online music stores and streaming platforms on March 23, 2020.</h3>
+            <h3>Available now on all of your favourite music streaming services. More links will be added later, but it's probably already available so just check for it on the streaming platform.</h3>
           </a>
 
+          <h2>Ellipsis (Remastered Deluxe), released in 2020:</h2>
+
+          <iframe title="Ellipsis (Remastered Deluxe) by Telephone Sound, embedded from YouTube." width="560" height="315" src="https://www.youtube-nocookie.com/embed/R1ZclqJiIjs" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+
+          <h3><a href="https://defcronyke.github.io/ts-camera-filter" target="_blank" rel="noopener noreferrer">Click here to try the Telephone Sound Audio-Reactive Camera Filter, you can use it with any device that has a screen, camera, and microphone attached. Visualize your surroundings with audio through this neat augmented reality web app.</a> <a href="https://github.com/defcronyke/ts-camera-filter" target="_blank" rel="noopener noreferrer">Click here for the source code.</a></h3>
+
           <ul>
+            <li><a href="https://telephonesound.bandcamp.com/album/ellipsis-remastered-deluxe" target="_blank" rel="noopener noreferrer">Stream or buy on BandCamp</a></li>
+            <li><a href="https://play.google.com/store/music/album/Telephone_Sound_Ellipsis_Remastered_Deluxe?id=Be6uvln5i3jngnc7hfw6qhk7mui" target="_blank" rel="noopener noreferrer">Stream or buy on Google Play Music Store</a></li>
+            <li><a href="https://www.amazon.com/gp/product/B084RR3J81" target="_blank" rel="noopener noreferrer">Stream or Buy on Amazon</a></li>
+            <li><a href="https://music.youtube.com/playlist?list=OLAK5uy_n2ppYqCNO2K_sb3KJFWy5z7M5LoMtM4L4" target="_blank" rel="noopener noreferrer">Stream on YouTube Music</a></li>
+            <li><a href="https://www.youtube.com/watch?v=R1ZclqJiIjs&list=OLAK5uy_n2ppYqCNO2K_sb3KJFWy5z7M5LoMtM4L4" target="_blank" rel="noopener noreferrer">Stream on YouTube</a></li>
+            <li><a href="https://open.spotify.com/album/4dDHYDQPaVzYEGLUAlvX6g" target="_blank" rel="noopener noreferrer">Stream on Spotify</a></li>
+            <li><a href="https://music.apple.com/ca/album/ellipsis-remastered-deluxe-remastered/1498888685" target="_blank" rel="noopener noreferrer">Stream on Apple Music</a></li>
+            <li><a href="https://www.deezer.com/album/131566232" target="_blank" rel="noopener noreferrer">Stream on Deezer</a></li>
+            {/* <li><a href="https://www.tidal.com/album/94767312" target="_blank" rel="noopener noreferrer">Stream on Tidal</a></li> */}
+            {/* <li><a href="https://us.napster.com/artist/telephone-sound/album/ellipsis-deluxe" target="_blank" rel="noopener noreferrer">Stream on Napster</a></li> */}
+            {/* <li><a href="https://www.kkbox.com/sg/en/album/wdKVeUJTvMSoj0F2MfQG009H-index.html" target="_blank" rel="noopener noreferrer">Stream on KKBox</a></li> */}
             <li><a href="https://www.facebook.com/telephonesound" target="_blank" rel="noopener noreferrer">Follow our Facebook page</a></li>
             <li><a href="https://www.patreon.com/telephonesound" target="_blank" rel="noopener noreferrer">Support us on Patreon</a></li>
             <li>Support us by sending an <a href="https://en.wikipedia.org/wiki/Interac_e-Transfer" target="_blank" rel="noopener noreferrer">Interac eTransfer</a> or <a href="https://paypal.com" target="_blank" rel="noopener noreferrer">PayPal transfer</a> to: <a href="mailto:jeremy@jeremycarter.ca">jeremy@jeremycarter.ca</a></li>
             <li>Support us by sending <a href="https://en.wikipedia.org/wiki/Bitcoin" target="_blank" rel="noopener noreferrer">Bitcoin</a> to BTC address: <a href="bitcoin:bc1quka4yv0tpfym8hpc34ljys0qfk92u5cljrfzxm?amount=0.0003">bc1quka4yv0tpfym8hpc34ljys0qfk92u5cljrfzxm</a></li>
+            <li><a href="https://archive.org/details/telephone-sound-ellipsis-remastered-deluxe" target="_blank" rel="noopener noreferrer">Stream or download on Archive.org</a></li>
           </ul>
 
           <div className="news">
