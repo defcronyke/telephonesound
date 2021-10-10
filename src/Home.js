f27a2e30201f101e345fdb52a03380abef35ac49
Jeremy Carter <jeremy@jeremycarter.ca>
2021-10-10 18:10:51 -0400

    https://chonkify

--- src/Home.js
+++ src/Home.js
@@ -25,7 +25,25 @@ export default class Home extends Component {
 				<br />
 				<br />
 			</div>
-			
+
+
+
+
+
+			<iframe alt="https://chonkify.me" title="minigame (for kids)" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Ftelephonesound%2Fposts%2F1132649737138810&show_text=true&width=500" width="500" height="543" scrolling="no" frameBorder="0" allowFullScreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" />
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -35,6 +53,10 @@ export default class Home extends Component {
 			<br /><br /><br />				
 			<br />      <br />
 			<br />      <br />
+
+
+
+
 
 
 
